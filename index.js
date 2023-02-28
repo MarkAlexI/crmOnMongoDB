ed5b15022b4d343b24831a9208eae76713cf09e7
MarkAlexI <85037509+MarkAlexI@users.noreply.github.com>
2023-02-28 07:29:00 +0000

    Create basic structure, added dependencies, starting work with express.js.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log(`Work!`);
+let express = require('express');
+let app = express();
+let bodyParser = require('body-parser');
+let http = require('http').Server(app);
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', function (req, res) {
+  res.sendFile('/index.html', {root:'.'});
+});
+
+app.get('/create', function (req, res) {
+  res.sendFile('/create.html', {root:'.'});
+});
+
+app.set('port', process.env.PORT || 5000);
+http.listen(app.get('port'), function() {
+    console.log('listening on port', app.get('port'));
+});
+
+const MongoClient = require('mongodb').MongoClient;
+const mongo_username = process.env.MONGO_USERNAME;
+const mongo_password = process.env.MONGO_PASSWORD;
+
+const uri = `mongodb+srv://${mongo_username}:${mongo_password}@crm.5vvsy1k.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true });
