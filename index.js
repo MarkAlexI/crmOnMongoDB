23575b4b92c9657adc4755279d9b3cdcf3065717
MarkAlexI <markovaleksandr108@gmail.com>
2023-03-11 18:49:56 +0000

    Fixed connect. Rewritten POST method.

--- index.js
+++ index.js
@@ -19,9 +19,30 @@ http.listen(app.get('port'), function() {
     console.log('listening on port', app.get('port'));
 });
 
-const MongoClient = require('mongodb').MongoClient;
 const mongo_username = process.env.MONGO_USERNAME;
 const mongo_password = process.env.MONGO_PASSWORD;
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${mongo_username}:${mongo_password}@crm.5vvsy1k.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true });
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+let conn, db;
+
+async function getDb() {
+  try {
+    conn = await client.connect();
+  } catch(e) {
+    console.error(e);
+  }
+
+  db = conn.db("crmdb");
+}
+getDb();
+
+app.post('/create', async function (req, res, next) {
+  let customer = { name: req.body.name, address: req.body.address, telephone: req.body.telephone, note: req.body.note };
+  const result = await db.collection("customers").insertOne(customer);
+  console.log(JSON.stringify(result.insertedId));
+
+  res.send('Customer created');
+});
