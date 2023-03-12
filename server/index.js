b8439f4897d0d99149b97186b91eaa6cb81b6bcf
MarkAlexI <markovaleksandr108@gmail.com>
2023-03-12 00:51:54 +0000

    Changed structure of project.

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+let db = require('./db/conn.js');
 let express = require('express');
 let app = express();
 let bodyParser = require('body-parser');
@@ -7,37 +8,25 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function (req, res) {
-  res.sendFile('/index.html', {root:'.'});
+  res.sendFile('/app/index.html', {root:'.'});
+});
+
+app.get('/assets/index.css', function (req, res) {
+  res.sendFile('/app/assets/index.css', {root:'.'});
 });
 
 app.get('/create', function (req, res) {
-  res.sendFile('/create.html', {root:'.'});
+  res.sendFile('/app/create.html', {root:'.'});
+});
+
+app.get('/create/assets/create.css', function (req, res) {
+  res.sendFile('/app/assets/create.css', {root:'.'});
 });
 
 app.set('port', process.env.PORT || 5000);
 http.listen(app.get('port'), function() {
     console.log('listening on port', app.get('port'));
 });
-
-const mongo_username = process.env.MONGO_USERNAME;
-const mongo_password = process.env.MONGO_PASSWORD;
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = `mongodb+srv://${mongo_username}:${mongo_password}@crm.5vvsy1k.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
-let conn, db;
-
-async function getDb() {
-  try {
-    conn = await client.connect();
-  } catch(e) {
-    console.error(e);
-  }
-
-  db = conn.db("crmdb");
-}
-getDb();
 
 app.post('/create', async function (req, res, next) {
   let customer = { name: req.body.name, address: req.body.address, telephone: req.body.telephone, note: req.body.note };
