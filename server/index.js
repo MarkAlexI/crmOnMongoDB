da6b92a1df92c74fa988fd5782f3f4861acb53a7
MarkAlexI <markovaleksandr108@gmail.com>
2023-03-15 15:28:10 +0000

    Added styles to index.html.

--- server/index.js
+++ server/index.js
@@ -1,8 +1,28 @@
-let db = require('./db/conn.js');
 let express = require('express');
 let app = express();
 let bodyParser = require('body-parser');
 let http = require('http').Server(app);
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const mongo_username = process.env.MONGO_USERNAME;
+const mongo_password = process.env.MONGO_PASSWORD;
+
+const uri = `mongodb+srv://${mongo_username}:${mongo_password}@crm.5vvsy1k.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+let conn, db;
+
+async function getDb() {
+  try {
+    conn = await client.connect();
+  } catch(e) {
+    console.error(e);
+  }
+
+  db = conn.db("crmdb");
+}
+
+getDb();
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -23,6 +43,10 @@ app.get('/create/assets/create.css', function (req, res) {
   res.sendFile('/app/assets/create.css', {root:'.'});
 });
 
+app.get('/records', function (req, res) {
+  res.send('Check');
+});
+
 app.set('port', process.env.PORT || 5000);
 http.listen(app.get('port'), function() {
     console.log('listening on port', app.get('port'));
@@ -32,6 +56,5 @@ app.post('/create', async function (req, res, next) {
   let customer = { name: req.body.name, address: req.body.address, telephone: req.body.telephone, note: req.body.note };
   const result = await db.collection("customers").insertOne(customer);
   console.log(JSON.stringify(result.insertedId));
-
-  res.send('Customer created');
+  res.redirect('/');
 });
